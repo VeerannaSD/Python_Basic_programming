902f3d872acefa73164c35b5c360b5da51647177
VeerannaSD <vsdhavaleshwar@gmail.com>
2022-08-15 12:26:32 +0530

    sum_of_digits_of_number.py

---
+++ sum_of_digits_of_number_anotherway.py
@@ -0,0 +1,22 @@
+'''
+Write a code get an integer number as input and print the sum of the digits.
+
+Input Description:
+A single line containing an integer.
+
+Output Description:
+Print the sum of the digits of the integer.
+
+Sample Input :
+124
+Sample Output :
+7
+'''
+N = int(input())
+sum = 0
+while N:
+    q = N//10
+    r = N%10
+    sum+=r
+    N=q
+print(sum)
