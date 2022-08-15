9411bb02df0cd43d343199dd9f740c1703728a43
VeerannaSD <vsdhavaleshwar@gmail.com>
2022-08-15 12:29:57 +0530

    Print the integer A, N times in separate line.

---
+++ Print_integer_n_number_of_times.py
@@ -0,0 +1,21 @@
+'''
+Write a code to get 2 integers A and N. Print the integer A, N times in separate line.
+
+Input Description:
+First line contains an integer A. Second line contains an Integer N.
+
+Output Description:
+Print the integer A, N times in a separate line.
+
+Sample Input :
+2 3
+Sample Output :
+2
+2
+2
+'''
+A,N = map(int,input().split())
+#print(A)
+#print(N)
+for i in range(N):
+    print(A)
